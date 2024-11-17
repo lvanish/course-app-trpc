02f379590c92aedbc7c768ef885a3ebdd9eff6e6
lvanish <lvanishkoul@gmail.com>
2024-11-17 22:39:31 +0530

    add

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { publicProcedure, router } from "./trpc";
+
+const todoInputType = z.object({
+  title: z.string(),
+  description: z.string(),
+});
+
+const appRouter = router({
+  createTodo: publicProcedure.input(todoInputType).mutation(async (opts) => {
+    const title = opts.input.title;
+    const description = opts.input.description;
+
+    // Do DB stuff here
+
+    return {
+      id: "1",
+    };
+  }),
+});
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
+export type AppRouter = typeof appRouter;
